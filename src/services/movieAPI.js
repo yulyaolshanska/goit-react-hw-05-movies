c6471182e5e94494ba3c01d3397782cbf8a26a74
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-07-27 19:56:25 +0300

    Работа с API

---
+++ src/services/movieAPI.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+const API_KEY = '5bad2d5f9d55e6c30d24d3c221da8172';
+
+export async function fetchTrandingFilms() {
+  const response = await axios.get(`/trending/movie/day?api_key=${API_KEY}`);
+  return response;
+}
+
+export async function fetchSearchingFilms(searchQuery) {
+  const response = await axios.get(
+    `/search/movie?api_key=${API_KEY}&query=${searchQuery}language=en-US?`
+  );
+  return response;
+}
+
+export async function fetchMovieDetails(id) {
+  const response = await axios.get(
+    `/movie/${id}?api_key=${API_KEY}&language=en-US?`
+  );
+  return response;
+}
+
+export async function getFilmReviews(id) {
+  const response = await axios.get(
+    `/movie/${id}/reviews?api_key=${API_KEY}&language=en-US?`
+  );
+  return response;
+}
+
+export async function getFilmCast(id) {
+  const response = await axios.get(
+    `/movie/${id}/credits?api_key=${API_KEY}&language=en-US?`
+  );
+  return response;
+}
