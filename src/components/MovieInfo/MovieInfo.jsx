27b5d415b11cffbc6866eead354ef87c3463c794
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-07-28 23:19:48 +0300

    Жанры

--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -1,16 +1,30 @@
-export const MovieInfo = ({ poster, title, overview, score }) => {
+export const MovieInfo = ({
+  poster,
+
+  score,
+  title,
+  overview,
+  genres,
+}) => {
+  // console.log('movies', movies);
+  // const { title, overview, genres } = movies;
+
+  // console.log('genres', genres);
+
+  // const joinGenres = genres.map(({ name }) => name);
   return (
     <>
       <div>
-        <img src={poster} alt={title}></img>
-      </div>
-      <div>
-        <h1>{title}</h1>
-        <p>user Score:{score}</p>
-        <h2>overview</h2>
-        <p>{overview}</p>
-        <h3>genres</h3>
-        <p></p>
+        <img src={poster} alt={title} width={'300px'}></img>
+
+        <div>
+          <h1>{title}</h1>
+          <p>User Score:{score}%</p>
+          <h2>Overview</h2>
+          <p>{overview}</p>
+          <h3>Genres</h3>
+          {genres && <p>{genres.map(({ name }) => name).join(', ')}</p>}
+        </div>
       </div>
     </>
   );
