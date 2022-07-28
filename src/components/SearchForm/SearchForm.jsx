19fe818383d502981027fb52fdb460a0565d1657
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-07-28 19:27:18 +0300

    SearchForm
    1. handleSubmit.
    2. handleChange.

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,8 +1,27 @@
-export const SearchForm = () => {
+import { useState } from 'react';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+export const SearchForm = ({ onSubmit }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (searchQuery === '') {
+      Notify.warning("You didn't enter anything to search");
+      return;
+    }
+
+    onSubmit(searchQuery);
+  };
+
+  const handleChange = e => {
+    setSearchQuery(e.target.value.toLowerCase().trim());
+  };
+
   return (
-    <form>
-      <input></input>
-      <button></button>
+    <form onSubmit={handleSubmit}>
+      <input onChange={handleChange}></input>
+      <button type="submit">Search</button>
     </form>
   );
 };
