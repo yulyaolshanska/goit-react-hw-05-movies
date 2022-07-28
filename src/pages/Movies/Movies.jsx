24835880da50754ef8f136728e0c9bf51c4e2584
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-07-28 19:27:51 +0300

    Movies
    1. Рендер фильмов по запросу.

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,11 +1,37 @@
 import { MovieList } from 'components/MovieList/MovieList';
 import { SearchForm } from 'components/SearchForm/SearchForm';
+import { useState, useEffect } from 'react';
+import * as movieAPI from 'services/movieAPI';
 
 export const Movies = () => {
+  const [query, setQuery] = useState('');
+  const [searchMovies, setSearchMovies] = useState([]);
+
+  useEffect(() => {
+    if (query === '') {
+      return;
+    }
+
+    movieAPI
+      .fetchSearchingFilms(query)
+      .then(setSearchMovies)
+      .catch(console.log);
+  }, [query, searchMovies]);
+
+  const getFormData = data => {
+    setQuery(data);
+  };
+
   return (
     <>
-      <SearchForm />
-      <MovieList />
+      <SearchForm onSubmit={getFormData} />
+      {searchMovies.length > 0 && <MovieList movies={searchMovies} />}
+      {query !== '' && searchMovies.length === 0 && (
+        <p>
+          Sorry, there are no movies matching your search query. Please try
+          again.
+        </p>
+      )}
     </>
   );
 };
