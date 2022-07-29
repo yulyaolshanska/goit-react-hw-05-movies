d9c952d9a97e818a403259dd33c7bd0022ba9b7a
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-07-29 20:04:19 +0300

    Styles for Movies

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,9 +1,9 @@
 import { MovieList } from 'components/MovieList/MovieList';
-// import { SearchForm } from 'components/SearchForm/SearchForm';
 import { useState, useEffect } from 'react';
 import * as movieAPI from 'services/movieAPI';
 import { useSearchParams } from 'react-router-dom';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import styles from './Movies.module.css';
 
 const Movies = () => {
   // const [query, setQuery] = useState('');
@@ -44,16 +44,19 @@ const Movies = () => {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <input
           type="text"
           name="query"
           onChange={handleChange}
           placeholder="Enter movie name"
+          className={styles.input}
         />
 
-        <button type="submit">Search</button>
+        <button type="submit" className={styles.formBtn}>
+          Search
+        </button>
       </form>
       {/* <SearchForm onSubmit={getFormData} /> */}
       {searchMovies.length > 0 && query && <MovieList movies={searchMovies} />}
@@ -63,7 +66,7 @@ const Movies = () => {
           again.
         </p>
       )}
-    </>
+    </div>
   );
 };
 
