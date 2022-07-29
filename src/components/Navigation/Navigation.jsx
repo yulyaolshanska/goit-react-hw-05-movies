8ba65633eb69315890cae829e4120d6b9d73a5ab
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-07-29 15:35:43 +0300

    Стили для Navigation
    1. Стилизация активной NavLink.

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,25 @@
 import { NavLink } from 'react-router-dom';
+import styles from './Navigation.module.css';
 
 export const Navigation = () => {
   return (
     <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/movies">Movies</NavLink>
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? styles.activeLink : styles.link
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/movies"
+        className={({ isActive }) =>
+          isActive ? styles.activeLink : styles.link
+        }
+      >
+        Movies
+      </NavLink>
     </nav>
   );
 };
