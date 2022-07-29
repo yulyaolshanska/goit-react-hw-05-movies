89ce509282da959c40c2a9434872cbb82508c6f6
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-07-29 20:03:35 +0300

    Styles for MovieItem

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,13 +1,24 @@
 import { NavLink, useLocation } from 'react-router-dom';
+import styles from './MovieItem.module.css';
+import PropTypes from 'prop-types';
 
 export const MovieItem = ({ title, id }) => {
   const location = useLocation();
 
   return (
-    <li>
-      <NavLink to={`/movies/${id}`} state={{ from: location }}>
+    <li className={styles.movieItem}>
+      <NavLink
+        className={styles.link}
+        to={`/movies/${id}`}
+        state={{ from: location }}
+      >
         {title}
       </NavLink>
     </li>
   );
+};
+
+MovieItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
 };
