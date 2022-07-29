0dd5582b7ae51d43db0a88d60d2db5b128a3bde4
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-07-29 15:32:49 +0300

    Styles AppBar

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,9 +1,10 @@
 import { Navigation } from '../Navigation/Navigation';
+import styles from './AppBar.module.css';
 
 export function AppBar() {
   return (
-    <>
+    <header className={styles.appBar}>
       <Navigation />
-    </>
+    </header>
   );
 }
