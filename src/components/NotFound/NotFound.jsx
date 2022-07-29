b185b57871c756e1f99bdbf9bc7a85cabb74579d
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-07-29 20:03:55 +0300

    Styles for NotFound

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
+import styles from './NotFound.module.css';
 
 const NotFound = () => {
   return (
-    <>
+    <div className={styles.container}>
       <h1>404 Page not found :(</h1>
-      <Link to="/">Home</Link>
-    </>
+      <Link to="/" className={styles.link}>
+        Home
+      </Link>
+    </div>
   );
 };
 
