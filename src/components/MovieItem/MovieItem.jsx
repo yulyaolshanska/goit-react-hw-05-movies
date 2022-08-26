bbd5212f111b962d704b119cb763a45fb5be633e
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-26 22:28:52 +0300

    Карточка фильма
    1.  poster, release, rating для карточек фильма.
    2. propTypes

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -2,8 +2,14 @@ import { NavLink, useLocation } from 'react-router-dom';
 import styles from './MovieItem.module.css';
 import PropTypes from 'prop-types';
 
-export const MovieItem = ({ title, id }) => {
+export const MovieItem = ({ title, id, poster, release, rating }) => {
   const location = useLocation();
+  const imgUrl = poster
+    ? ` https://image.tmdb.org/t/p/w300${poster}`
+    : 'https://www.freeiconspng.com/uploads/no-image-icon-6.png';
+
+  const releaseYear = release.slice(0, 4);
+  const movieRating = rating.toFixed(1);
 
   return (
     <li className={styles.movieItem}>
@@ -12,7 +18,12 @@ export const MovieItem = ({ title, id }) => {
         to={`/movies/${id}`}
         state={{ from: location }}
       >
-        {title}
+        <img src={imgUrl} alt={title} />
+        <div className={styles.movieInfo}>
+          <h2>{title}</h2>
+          <p>{releaseYear}</p>
+          <p>{movieRating}</p>
+        </div>
       </NavLink>
     </li>
   );
@@ -21,4 +32,7 @@ export const MovieItem = ({ title, id }) => {
 MovieItem.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  release: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
 };
